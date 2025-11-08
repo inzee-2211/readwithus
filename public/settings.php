51df5c1b793a581bb27b3cf0a36a3955bfc7cbd5
Rehan Khan <rehan10crkt@gmail.com>
2025-11-08 21:55:57 +0500

    Update settings for production environment

--- public/settings.php
+++ public/settings.php
@@ -1,9 +1,8 @@
 <?php
-// public/settings.php - UPDATED
-defined('CONF_WEBROOT_FRONTEND')  || define('CONF_WEBROOT_FRONTEND',  'http://localhost/readwithus/readwithus/public/');
-defined('CONF_WEBROOT_DASHBOARD') || define('CONF_WEBROOT_DASHBOARD', 'http://localhost/readwithus/readwithus/public/dashboard/');
-defined('CONF_WEBROOT_BACKEND')   || define('CONF_WEBROOT_BACKEND',   'http://localhost/readwithus/readwithus/public/admin/');
-defined('CONF_DB_SERVER') || define('CONF_DB_SERVER', '127.0.0.1');
-defined('CONF_DB_USER')   || define('CONF_DB_USER',   'root');
-defined('CONF_DB_PASS')   || define('CONF_DB_PASS',   '');
-defined('CONF_DB_NAME')   || define('CONF_DB_NAME',   'readwithus2'); // use the DB you actually imported
+define('CONF_WEBROOT_FRONTEND', 'https://readwithus.org.uk/');
+define('CONF_WEBROOT_DASHBOARD', 'https://readwithus.org.uk/dashboard/');
+define('CONF_WEBROOT_BACKEND', '/admin/');
+defined('CONF_DB_SERVER') || define('CONF_DB_SERVER','localhost');
+defined('CONF_DB_USER') || define('CONF_DB_USER', 'debian-sys-maint');
+defined('CONF_DB_PASS') || define('CONF_DB_PASS', 'bXHt3zI8AhsbKbf4');
+defined('CONF_DB_NAME') || define('CONF_DB_NAME', 'readwithus_test_v1');
