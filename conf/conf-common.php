a58d9dc582bb1a9ecbbb19c6c875ddc616ab70df
Rehan <rehan10crkt@gmail.com>
2025-12-12 21:22:16 +0500

    stripe

--- conf/conf-common.php
+++ conf/conf-common.php
@@ -21,9 +21,9 @@ define('CONF_ZOOM_VERSION', '2.5.0');
 define('SEARCH_MAX_COUNT', 10000);
 define('CONF_UPLOADS_PATH', CONF_INSTALLATION_PATH . 'user-uploads' . DIRECTORY_SEPARATOR);
 define('CONF_EDITOR_PATH', '/user-uploads/editor');
-define('CONF_STRIPE_SECRET_KEY', 'sk_test_51SSDb7FP4feQExSlXeRFcSIyotbnP5Z2HU2qkckU5C5JFiXNuiebIpX8Lb7TZzcDnpNngAQBrnzlmOisSTiN5vGw00xh0B9QPj'); // Your test secret key
+define('CONF_STRIPE_SECRET_KEY', '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'); // Your test secret key
 define('CONF_STRIPE_PUBLISHABLE_KEY', 'pk_live_51JHBeKJxDI9aGftHgMx65kEksngoOmFbwLkFGM3PkP3eH3yPEkOXfeIWWvW5qnjGy8X35eDfrfZrkZHWZodrpi9k00rhloZDDg'); // Your test publishable key
-define('CONF_STRIPE_WEBHOOK_SECRET', 'whsec_905d926b1df7aef9b8c74d9312a00df1a5fc2388d6c99dbc39f56e7f0ae39c40'); // Your webhook secret
+define('CONF_STRIPE_WEBHOOK_SECRET', 'whsec_yDyfgKtahphw1WanMAztrz1MYFlDA4hn'); // Your webhook secret
 define('CONF_ENABLE_MATH_EDITOR', 1); // 1 = enabled, 0 = disabled
 define('CONF_MATH_KEYBOARD_DEFAULT', 'basic'); // basic, numeric, advanced
 define('CONF_MATH_MAX_LATEX_LENGTH', 5000); // Maximum LaTeX characters
