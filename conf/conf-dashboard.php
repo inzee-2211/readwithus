1a23c354e8ff19c1d6c9055c92f914500486546c
Rehan <rehan10crkt@gmail.com>
2026-01-18 21:21:57 +0500

    maintainance

--- conf/conf-dashboard.php
+++ conf/conf-dashboard.php
@@ -10,3 +10,4 @@ define('SYSTEM_FRONT', true);
 define('CONF_HTML_EDITOR', 'innova');
 define('CONF_FAT_CACHE_DIR', CONF_INSTALLATION_PATH . 'public/cache/');
 define('CONF_FAT_CACHE_URL', CONF_WEBROOT_FRONTEND . 'cache/');
+define('CONF_LIVE_LESSONS_MAINTENANCE', 1);
