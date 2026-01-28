2ff2f3a279ef2a53a2f03b71a0c45a83ff85d055
Rehan <rehan10crkt@gmail.com>
2026-01-28 22:27:58 +0500

    certificate download issue fixed

--- manager/views/teacher-requests/search-qualifications.php
+++ manager/views/teacher-requests/search-qualifications.php
@@ -1,4 +1,8 @@
 <?php defined('SYSTEM_INIT') or die('Invalid Usage.'); ?>
+<style>
+    .tablewrap a { pointer-events: auto !important; }
+
+</style>
 <section class="section">
     <div class="sectionhead">
         <h4><?php echo Label::getLabel('LBL_TEACHER_REQUEST_DETAIL'); ?></h4>
@@ -33,18 +37,37 @@
                             case 'uqualification_experience_type':
                                 $td->appendElement('plaintext', [], UserQualification::getExperienceTypeArr()[$row['uqualification_experience_type']] . '<br/>' . $row['uqualification_start_year'] . '-' . $row['uqualification_end_year'], true);
                                 break;
+                            // case 'certificate_file':
+                            //     $td->appendElement('span', ['class' => 'td__caption -hide-desktop -show-mobile'], Label::getLabel('LBL_CERTIFICATE'));
+                            //     $span = $td->appendElement('span', ['class' => 'td__data'], '');
+                            //     if (!empty($row['file_id'])) {
+                            //         $a = $span->appendElement('a', ["target" => "_blank", 'href' => MyUtility::makeFullUrl('Image', 'download', [Afile::TYPE_USER_QUALIFICATION_FILE, $row['uqualification_id']])], '');
+                            //         $divInsideSpan = $a->appendElement('div', ['class' => 'attachment-file'], '');
+                            //         $spanInside_DivInsideSpan = $divInsideSpan->appendElement('div', ['class' => 'inline-icon -display-inline -color-fill']);
+                            //         $svgSpan = $spanInside_DivInsideSpan->appendElement('span', ['class' => 'svg-icon']);
+                            //         $svgSpan->appendElement('plaintext', [], '<img src="' . CONF_WEBROOT_BACKEND . 'images/attach.svg" class="">', true);
+                            //         $divInsideSpan->appendElement('plaintext', [], $row['file_name']);
+                            //     }
+                            //     break;
                             case 'certificate_file':
-                                $td->appendElement('span', ['class' => 'td__caption -hide-desktop -show-mobile'], Label::getLabel('LBL_CERTIFICATE'));
-                                $span = $td->appendElement('span', ['class' => 'td__data'], '');
-                                if (!empty($row['file_id'])) {
-                                    $a = $span->appendElement('a', ["target" => "_blank", 'href' => MyUtility::makeFullUrl('Image', 'download', [Afile::TYPE_USER_QUALIFICATION_FILE, $row['uqualification_id']])], '');
-                                    $divInsideSpan = $a->appendElement('div', ['class' => 'attachment-file'], '');
-                                    $spanInside_DivInsideSpan = $divInsideSpan->appendElement('div', ['class' => 'inline-icon -display-inline -color-fill']);
-                                    $svgSpan = $spanInside_DivInsideSpan->appendElement('span', ['class' => 'svg-icon']);
-                                    $svgSpan->appendElement('plaintext', [], '<img src="' . CONF_WEBROOT_BACKEND . 'images/attach.svg" class="">', true);
-                                    $divInsideSpan->appendElement('plaintext', [], $row['file_name']);
-                                }
-                                break;
+    $td->appendElement('span', ['class' => 'td__caption -hide-desktop -show-mobile'], Label::getLabel('LBL_CERTIFICATE'));
+    $span = $td->appendElement('span', ['class' => 'td__data'], '');
+
+    if (!empty($row['file_id'])) {
+        $url = MyUtility::makeFullUrl('Image', 'download', [Afile::TYPE_USER_QUALIFICATION_FILE, $row['uqualification_id']]);
+
+        // ✅ Make the <a> the actual clickable element (block) and keep it above overlays
+        $a = $span->appendElement('a', [
+            'href' => $url,
+            'target' => '_blank',
+            'style' => 'display:inline-flex;align-items:center;gap:6px;position:relative;z-index:5;pointer-events:auto;',
+        ], '', false);
+
+        $a->appendElement('plaintext', [], '<img src="' . CONF_WEBROOT_BACKEND . 'images/attach.svg" style="width:16px;height:16px;">', true);
+        $a->appendElement('plaintext', [], $row['file_name'], true);
+    }
+    break;
+
                             case 'uqualification_institute_name':
                                 $td->appendElement('plaintext', [], $row['uqualification_institute_name'] . '<br/>' . $row['uqualification_institute_address'], true);
                                 break;
