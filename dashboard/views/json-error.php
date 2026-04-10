df16b17dd4a85c205a8338b8337ab7fdd32769ba
inzee-2211 <malikinzmam92@gmail.com>
2026-04-10 23:10:46 +0500

    Delete dashboard directory

--- dashboard/views/json-error.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-defined('SYSTEM_INIT') or die('Invalid Usage.');
-FatUtility::dieJsonError($this->variables);
--- dashboard/views/tutorials/no-quiz.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSTEM_INIT') or die('Invalid Usage.'); ?>
-
-<div class="message-display no-skin">
-    <div class="message-display__media">
-        <svg><use xlink:href="<?php echo CONF_WEBROOT_DASHBOARD ?>images/sprite.svg#stuck"></use></svg>
-    </div>
-    <h4><?php echo $msg ?? Label::getLabel("LBL_NO_QUIZ_AVAILABLE_FOR_THIS_LECTURE"); ?></h4>
-</div>
